ae89aa36197d48f9dd5e5a5e7345b3169055ddb2
lokinsethia <alphabetagamma42@gmail.com>
2021-04-27 00:58:54 +0530

    heroku databse

--- code_predict/settings.py
+++ code_predict/settings.py
@@ -85,7 +85,10 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+import dj_database_url
 
+db_env = dj_database_url.config(conn_max_age=600)
+DATABASES["default"].update(db_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
